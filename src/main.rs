c58309a74378748574cefabd3fa51491fcd1d360
peter bull <17122363+peterbull@users.noreply.github.com>
2025-12-17 04:54:17 -0500

    init commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#![allow(unused_variables)]
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} tokenize <filename>", args[0]);
+        return;
+    }
+
+    let command = &args[1];
+    let filename = &args[2];
+
+    match command.as_str() {
+        "tokenize" => {
+            eprintln!("Logs from your program will appear here!");
+
+            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+                eprintln!("Failed to read file {}", filename);
+                String::new()
+            });
+
+            if !file_contents.is_empty() {
+                panic!("Scanner not implemented");
+            } else {
+                println!("EOF  null");
+            }
+        }
+        _ => {
+            eprintln!("Unknown command: {}", command);
+        }
+    }
+}
