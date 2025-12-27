0900f06950045685aad730699ae279bdedf4f341
peter bull <17122363+peterbull@users.noreply.github.com>
2025-12-27 10:55:08 -0500

    add repl

--- src/lox.rs
+++ src/lox.rs
@@ -1,10 +1,29 @@
+use std::io::{self, Write};
+
 use crate::scanner::Scanner;
 
 pub struct Lox {}
 impl Lox {
-    pub fn run(text: &str) {
+    pub fn new() -> Self {
+        Lox {}
+    }
+    pub fn run(&self, text: &str) {
         let mut scanner = Scanner::new(text.to_string());
         scanner.scan_tokens();
         scanner.print_info();
+    }
+    pub fn run_repl(&self) -> io::Result<()> {
+        println!("Starting REPL...");
+        loop {
+            print!("> ");
+            io::stdout().flush()?;
+            let mut input_text = String::new();
+            io::stdin().read_line(&mut input_text)?;
+            if input_text.trim() == "exit" {
+                break;
+            }
+            self.run(&input_text);
+        }
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,28 @@ use std::fs;
 
 use lox_interpreter::lox::Lox;
 
-fn scanner_error(line: usize, msg: String) {
-    println!("ERROR: Line {}, {}", line, msg);
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: {} tokenize <filename>", args[0]);
-        return;
-    }
 
     let command = &args[1];
-    let filename = &args[2];
-
+    let lox = Lox::new();
     match command.as_str() {
         "tokenize" => {
-            eprintln!("Logs from your program will appear here!");
-
+            let filename = &args[2];
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 eprintln!("Failed to read file {}", filename);
                 String::new()
             });
 
             if !file_contents.is_empty() {
-                Lox::run(&file_contents);
+                lox.run(&file_contents);
                 // panic!("Scanner not implemented");
             } else {
                 println!("EOF  null");
             }
+        }
+        "repl" => {
+            let _ = lox.run_repl();
         }
         _ => {
             eprintln!("Unknown command: {}", command);
