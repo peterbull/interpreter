8e1e6da55005bff7754ac8a4af514e0153f43773
peter bull <17122363+peterbull@users.noreply.github.com>
2025-12-28 12:50:32 -0500

    set up expression enum

---
+++ src/expr.rs
@@ -0,0 +1,86 @@
+#![allow(unused_variables, dead_code)]
+use crate::{Literal, Token};
+
+pub enum Expr {
+    Assign {
+        name: Token,
+        value: Box<Expr>,
+    },
+    Binary {
+        left: Box<Expr>,
+        operator: Token,
+        right: Box<Expr>,
+    },
+    Call {
+        callee: Box<Expr>,
+        token: Token,
+        arguments: Vec<Expr>,
+    },
+    Get {
+        object: Box<Expr>,
+        name: Token,
+    },
+    Grouping {
+        expression: Box<Expr>,
+    },
+    Literal {
+        value: Literal,
+    },
+    Logical {
+        left: Box<Expr>,
+        operator: Token,
+        right: Box<Expr>,
+    },
+    Set {
+        object: Box<Expr>,
+        name: Token,
+        value: Box<Expr>,
+    },
+    Super {
+        keyword: Token,
+        method: Token,
+    },
+    This {
+        keyword: Token,
+    },
+    Unary {
+        operator: Token,
+        right: Box<Expr>,
+    },
+    Variable {
+        name: Token,
+    },
+}
+
+pub fn evaluate(expr: &Expr) {
+    match expr {
+        Expr::Assign { name, value } => {}
+        Expr::Binary {
+            left,
+            operator,
+            right,
+        } => {}
+        Expr::Call {
+            callee,
+            token,
+            arguments,
+        } => {}
+        Expr::Get { object, name } => {}
+        Expr::Grouping { expression } => {}
+        Expr::Literal { value } => {}
+        Expr::Logical {
+            left,
+            operator,
+            right,
+        } => {}
+        Expr::Set {
+            object,
+            name,
+            value,
+        } => {}
+        Expr::Super { keyword, method } => {}
+        Expr::This { keyword } => {}
+        Expr::Unary { operator, right } => {}
+        Expr::Variable { name } => {}
+    }
+}
